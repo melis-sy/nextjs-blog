4bb39afc8b7dcc6650d4ef573425fea8539d6d28
melis-sy <74507515+melis-sy@users.noreply.github.com>
2022-08-21 16:32:42 +0200

    Anpassungen Test

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,6 +3,7 @@ import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head';
 import Date from '../../components/date';
 import utilStyles from '../../styles/utils.module.css';
+import Button from '@mui/material/Button';
 
 
 
