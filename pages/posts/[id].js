54469e9af9b6fe0d957257ce48f3e94ed25673a6
melis-sy <74507515+melis-sy@users.noreply.github.com>
2022-09-02 10:31:33 +0200

    Weiterleitung wenn Post gelöscht ist

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -5,27 +5,34 @@ import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Buttongroup from "../../components/buttonGroup";
+import { useRouter } from "next/router";
 
 const theme = createTheme({});
 
 export default function Post({ postData }) {
-  return (
-    <ThemeProvider theme={theme}>
-      <Layout>
-        <Buttongroup id={postData.id} />
-        <Head>
-          <title>{postData.title}</title>
-        </Head>
-        <article>
-          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-          <div className={utilStyles.lightText}>
-            <Date dateString={postData.date} />
-          </div>
-          <div dangerouslySetInnerHTML={{ __html: postData.content }}></div>
-        </article>
-      </Layout>
-    </ThemeProvider>
-  );
+  const router = useRouter();
+
+  if (postData.isDeleted) {
+    router.push(`/posts/deleted-posts`);
+  } else {
+    return (
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Buttongroup id={postData.id} />
+          <Head>
+            <title>{postData.title}</title>
+          </Head>
+          <article>
+            <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+            <div className={utilStyles.lightText}>
+              <Date dateString={postData.date} />
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: postData.content }}></div>
+          </article>
+        </Layout>
+      </ThemeProvider>
+    );
+  }
 }
 
 export async function getStaticPaths() {
